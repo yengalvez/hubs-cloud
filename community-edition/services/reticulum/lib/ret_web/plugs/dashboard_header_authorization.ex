d4edbc2c53eacff88684e1392c7d5d614a220942
Virtualmente <info@virtualmene.com>
2026-02-12 01:16:43 +0100

    Fallback dashboard auth key when placeholder is unresolved

--- community-edition/services/reticulum/lib/ret_web/plugs/dashboard_header_authorization.ex
+++ community-edition/services/reticulum/lib/ret_web/plugs/dashboard_header_authorization.ex
@@ -5,9 +5,16 @@ defmodule RetWeb.Plugs.DashboardHeaderAuthorization do
   def init(default), do: default
 
   def call(conn, _default) do
+    configured_value =
+      Application.get_env(:ret, RetWeb.Plugs.DashboardHeaderAuthorization)[:dashboard_access_key]
+
     expected_value =
-      Application.get_env(:ret, RetWeb.Plugs.DashboardHeaderAuthorization)[:dashboard_access_key] ||
-        Application.get_env(:ret, :bot_access_key)
+      case configured_value do
+        nil -> Application.get_env(:ret, :bot_access_key)
+        "" -> Application.get_env(:ret, :bot_access_key)
+        "<DASHBOARD_ACCESS_KEY>" -> Application.get_env(:ret, :bot_access_key)
+        value -> value
+      end
 
     case conn |> get_req_header(@header_name) do
       [""] -> conn |> send_resp(401, "") |> halt()
